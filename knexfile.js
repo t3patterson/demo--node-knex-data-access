c97333424a96540d4489dfa66e8fd4d342cf19ba
Travis H <t3patterson@gmail.com>
2017-11-14 17:48:33 -0600

    Update knexfile.js

--- knexfile.js
+++ knexfile.js
@@ -1,1 +1,21 @@
-module.exports = require('./config/dbAccess.js')
+const devConfig = {
+  client: 'mysql2',
+  connection: {
+    host : '127.0.0.1',
+    port: '3306',
+    user : '------your-db-username-----',
+    password : '------your-db-user-password------',
+    database : '-----yourdb-----'
+  },
+  migrations: {
+    directory: './src/db/migrations'
+  },
+  seeds: {
+    directory: './src/db/seeds'
+  }
+}
+
+module.exports = {
+  development: devConfig,
+  production: {}
+}
